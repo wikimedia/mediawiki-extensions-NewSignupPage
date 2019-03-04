c05a8a4b98cb3493069a4b324ea8d55f85370687
Jack Phoenix <ashley@uncyclomedia.co>
2019-03-04 04:14:17 +0200

    Optimization -- drop pre-1.27 compatibility code; the core signup page is canonically Special:CreateAccount nowadays
    Change-Id: Id92491cf6ba76093591d72bca572cc646e07cbaf

--- includes/NewSignupPage.class.php
+++ includes/NewSignupPage.class.php
@@ -2,12 +2,10 @@
 /**
  * NewSignupPage extension for MediaWiki -- enhances the default signup form
  *
- * All class methods are public and static.
- *
  * @file
  * @ingroup Extensions
  * @author Jack Phoenix
- * @copyright Copyright © 2008-2017 Jack Phoenix
+ * @copyright Copyright © 2008-2019 Jack Phoenix
  * @license GPL-2.0-or-later
  */
 class NewSignupPage {
@@ -17,37 +15,15 @@ class NewSignupPage {
 	 *
 	 * @param OutputPage $out
 	 * @param Skin $skin
-	 * @return bool
 	 */
 	public static function onBeforePageDisplay( &$out, &$skin ) {
-		$context = $out;
-		$title = $context->getTitle();
-		$request = $context->getRequest();
+		$title = $out->getTitle();
 
 		// Only do our magic if we're on the signup page
 		if ( $title->isSpecial( 'CreateAccount' ) ) {
 			// It's called Special:CreateAccount since AuthManager (MW 1.27+)
 			$out->addModules( 'ext.newsignuppage' );
-		} elseif ( $title->isSpecial( 'Userlogin' ) ) {
-			$kaboom = explode( '/', $title->getText() );
-			$signupParamIsSet = false;
-
-			// Catch [[Special:UserLogin/signup]]
-			if ( isset( $kaboom[1] ) && $kaboom[1] == 'signup' ) {
-				$signupParamIsSet = true;
-			}
-
-			// Both index.php?title=Special:UserLogin&type=signup and
-			// Special:UserLogin/signup are valid, obviously
-			if (
-				$request->getVal( 'type' ) == 'signup' ||
-				$signupParamIsSet
-			) {
-				$out->addModules( 'ext.newsignuppage' );
-			}
 		}
-
-		return true;
 	}
 
 }
