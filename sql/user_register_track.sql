3c928640d226dabf75efbf571797f8f8e0792400
Jack Phoenix <ashley@uncyclomedia.co>
2021-05-06 10:29:51 +0300

    For SQLite compatibility, PRIMARY KEY needs to be *before* AUTO_INCREMENT
    Follow-up to 5bbbb975ebfd6044972472daa5cf72a4d78d6f65.
    
    Change-Id: I0ea4cdb6c796bb14face46bd7485f4016b4c1b3b

--- sql/user_register_track.sql
+++ sql/user_register_track.sql
@@ -1,5 +1,5 @@
 CREATE TABLE /*_*/user_register_track (
-  `ur_id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `ur_id` int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `ur_actor` bigint unsigned NOT NULL,
   `ur_actor_referral` bigint unsigned NOT NULL,
   `ur_from` int(5) default 0,
